454c88457ad0083fd22a39d271478be60e197ad3
Abir Sheikh <abir.sheikh.4044@gmail.com>
2026-04-23 13:27:26 +0600

    🧪 added test cases 🐛 fixed found bug

--- src/lib/tokenizer.rs
+++ src/lib/tokenizer.rs
@@ -69,8 +69,8 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, DeserializationError> {
                 let mut true_acum = String::with_capacity("true".len());
                 true_acum.push('t');
                 for _ in 1..=("true".len() - 1) {
-                    if let Some((_, part_of_null)) = iterable_input.next() {
-                        true_acum.push(part_of_null);
+                    if let Some((_, part_of_true)) = iterable_input.next() {
+                        true_acum.push(part_of_true);
                     } else {
                         return Err(UNEXPECTED_EOF);
                     }
@@ -82,19 +82,19 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, DeserializationError> {
                 }
             }
 
-            // handle null
+            // handle false
             'f' => {
                 let mut false_acum = String::with_capacity("false".len());
                 false_acum.push('f');
                 for _ in 1..=("false".len() - 1) {
-                    if let Some((_, part_of_null)) = iterable_input.next() {
-                        false_acum.push(part_of_null);
+                    if let Some((_, part_of_false)) = iterable_input.next() {
+                        false_acum.push(part_of_false);
                     } else {
                         return Err(UNEXPECTED_EOF);
                     }
                 }
                 if false_acum == "false" {
-                    token_accum.push(Token::Null)
+                    token_accum.push(Token::Boolean(false));
                 } else {
                     return Err(UNEXPECTED_TOKEN("unexpected token f".into()));
                 }
@@ -105,12 +105,12 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, DeserializationError> {
                     continue;
                 }
                 // handle numbers
-                if (other == '-') || (other.is_ascii_digit()) {
+                if (other == '-') || (other.is_ascii_digit()) || (other == '.') {
                     let mut num_accum = String::from(other);
                     // loops until next char is something other than digit
                     // if we called .next() here, we would lose the character that comes right after the number part ends
                     'extrct_num: while let Some((_, part_of_num)) = iterable_input.peek() {
-                        if part_of_num.is_ascii_digit() {
+                        if part_of_num.is_ascii_digit() || (part_of_num == &'.') {
                             // if next char was a digit, push it to num accumulator and call next as .peek() won't advance our uterator
                             num_accum.push(*part_of_num);
                             iterable_input.next();
@@ -132,4 +132,149 @@ pub fn tokenize(input: String) -> Result<Vec<Token>, DeserializationError> {
         }
     }
     return Ok(token_accum);
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::{tokenize, Token::*, DeserializationError::*};
+    #[test]
+    fn tokenization_only_l_brace(){
+        let input: String = "{".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![LeftBrace]));
+    }
+
+    #[test]
+    fn tokenization_only_r_brace(){
+        let input: String = "}".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![RightBrace]));
+    }
+
+    #[test]
+    fn tokenization_only_l_bracket(){
+        let input: String = "[".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![LeftBracket]));
+    }
+
+    #[test]
+    fn tokenization_only_r_bracket(){
+        let input: String = "]".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![RightBracket]));
+    }
+
+      #[test]
+    fn tokenization_only_colon(){
+        let input: String = ":".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Colon]));
+    }
+
+
+      #[test]
+    fn tokenization_only_comma(){
+        let input: String = ",".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Comma]));
+    }
+
+    #[test]
+    #[should_panic]
+    fn tokenization_only_doublequote(){
+        let input: String = "\"".to_string();
+        let tokens = tokenize(input);
+        tokens.unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn tokenization_unfinished_string(){
+        let input: String = "\"name".to_string();
+        let tokens = tokenize(input);
+        tokens.unwrap();
+    }
+
+    #[test]
+    fn tokenization_only_string(){
+        let input: String = "\"test_string\"".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![DoubleQuote, Str("test_string".into()), DoubleQuote]));
+    }
+
+    #[test]
+    fn tokenization_only_number(){
+        let input: String = "90".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(90f64)]));
+    }
+
+
+    #[test]
+    fn tokenization_only_number_with_radix(){
+        let input: String = "90.09".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(90.09f64)]));
+    }
+
+
+    #[test]
+    fn tokenization_only_number_starting_with_radix(){
+        let input: String = ".08".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(0.08f64)]));
+    }
+
+
+    #[test]
+    fn tokenization_only_negative_number(){
+        let input: String = "-100".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(-100f64)]));
+    }
+
+    #[test]
+    fn tokenization_only_negative_number_with_radix(){
+        let input: String = "-10.10".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(-10.1f64)]));
+    }
+
+    #[test]
+    fn tokenization_only_negative_number_immediately_followed_by_radix(){
+        let input: String = "-.10".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Number(-0.1f64)]));
+    }
+
+    #[test]
+    fn tokenization_only_bool_true(){
+        let input: String = "true".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Boolean(true)]));
+    }
+
+    #[test]
+    fn tokenization_only_bool_false(){
+        let input: String = "false".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Boolean(false)]));
+    }
+
+    #[test]
+    fn tokenization_only_null(){
+        let input: String = "null".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![Null]));
+    }
+
+
+    #[test]
+    fn tokenization_all_tokens(){
+        let input: String = "{}[]\"name\":,-100 true false null".to_string();
+        let tokens = tokenize(input);
+        assert_eq!(tokens, Ok(vec![LeftBrace, RightBrace, LeftBracket, RightBracket, DoubleQuote, Str("name".into()), DoubleQuote, Colon, Comma, Number(-100.0), Boolean(true), Boolean(false), Null]));
+    }
 }
